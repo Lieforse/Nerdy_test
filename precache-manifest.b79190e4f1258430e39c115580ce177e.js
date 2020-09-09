766de412275f7e35b4a3000f1e14b4e6bdfbe16c
lieforse <lieforse@gmail.com>
2020-09-09 15:06:06 +0300

    Updates

---
+++ precache-manifest.b79190e4f1258430e39c115580ce177e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bedfd4374847ad1ae61f41bc7c841e0",
+    "url": "/Nerdy_test/index.html"
+  },
+  {
+    "revision": "e1ae45a5aa64ab6316aa",
+    "url": "/Nerdy_test/static/css/main.49d64155.chunk.css"
+  },
+  {
+    "revision": "8a587c9304259d77a7a0",
+    "url": "/Nerdy_test/static/js/2.f5227df1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nerdy_test/static/js/2.f5227df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ae45a5aa64ab6316aa",
+    "url": "/Nerdy_test/static/js/main.bd2b7b88.chunk.js"
+  },
+  {
+    "revision": "d227f15e9f5f6795cbcf",
+    "url": "/Nerdy_test/static/js/runtime-main.f6980b70.js"
+  }
+]);
